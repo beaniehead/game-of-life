785ad8a21c481700832bb4668aaf4b93401f339b
beaniehead <17915329+beaniehead@users.noreply.github.com>
2018-01-25 10:41:35 -0500

    Minor edits

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import gridLayout from "./gridLayout";
+//import gridLayout from "./gridLayout";
 import Controls from "./Controls";
 import Grid from "./Grid";
 
