7a393fda6bdd26c74f9af1f10df0e5a9c2b2a9b5
beaniehead <17915329+beaniehead@users.noreply.github.com>
2018-01-24 23:41:09 -0500

    Added random grid and start

--- src/App.js
+++ src/App.js
@@ -9,18 +9,41 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      grid: gridLayout,
-      gameStatus: "paused"
+      grid: [],
+      gameStatus: "paused",
+      gridSize: 35,
+      speed: 150
+      //use to determine size of grid at start. New grid sizes stored here, but maybe different function to one in willmount, as new grid will be empty
+      // maybe use nested loops and simple push new grid to array
     }
     this.editGrid = this.editGrid.bind(this);
     this.runGame = this.runGame.bind(this);
     this.startGame = this.startGame.bind(this);
     this.pauseGame = this.pauseGame.bind(this);
     this.resetGame = this.resetGame.bind(this);
-
   }
 
+  componentWillMount() {
+    const size = this.state.gridSize;
+    const assign = {
+      1: true,
+      2: false
+    };
+    const grid = [];
+    for (let j = 0; j < size; j += 1) {
+      const newArray = [];
+      for (let i = 0; i < size; i += 1) {
+        const rand = Math.floor(Math.random() * 2) + 1;
+        newArray.push(assign[rand]);
+      }
+      grid.push(newArray);
+    }
+    this.setState({ grid });
+  }
 
+  componentDidMount() {
+    setTimeout(this.startGame, 1000);
+  }
   editGrid(e) {
     const coord = e.target.id.split("-");
     const row = +coord[0];
@@ -32,7 +55,7 @@ class App extends React.Component {
     // Set state using the now updated newGrid
     this.setState({
       grid: newGrid
-    })
+    });
   }
 
   startGame() {
@@ -40,20 +63,20 @@ class App extends React.Component {
     const intervalID = setInterval(() => {
       this.runGame();
       // i++;
-    }, 100); //Speed
+    }, this.state.speed); //Speed
     this.setState({ intervalID });
   }
 
   runGame() {
     // Get current grid status
     const gridStatus = [...this.state.grid];
-    //Need to assess the status of the eight neighbours of each cell for every cell and store the value in arrays
-    // Map through each cell, using its row and cell number to 
+    // Need to assess the status of the eight neighbours of each cell for every cell and store the value in arrays
+    // Map through each cell, using its row and cell number to
     const grid = gridStatus.map(gridRow =>
       gridRow.map((cell, index) => {
         let row = gridStatus.indexOf(gridRow);
         let column = index;
-        //Need to get status of neighbours. 
+        // Need to get status of neighbours.
         // Row -1: col-1, col=, col+1
         // Row  =: col-1, col+1
         // Row +1: col-1, col=, col+1
@@ -79,8 +102,8 @@ class App extends React.Component {
             }
             // logic here for comparing all the neighbours and calculated how many are alive
             // if (gridStatus[compareRow][compareColumn] === true || gridStatus[compareRow][compareColumn] === "true-old"||gridStatus[compareRow][compareColumn] === "true-mature") {
-            // if (gridStatus[compareRow][compareColumn] === true || gridStatus[compareRow][compareColumn] === "true-old") {
-            if (gridStatus[compareRow][compareColumn] === true) {
+            if (gridStatus[compareRow][compareColumn] === true || gridStatus[compareRow][compareColumn] === "true-old") {
+              // if (gridStatus[compareRow][compareColumn] === true) {
               alive++;
             }
 
@@ -89,7 +112,7 @@ class App extends React.Component {
         //Above loops calculate all cells, including current cell, so - one from total alive to see how many neighbouring cells are alive
         // TODO Can return on 4 in the loops as this is the max needed to calculate status
         alive = cell === true ? alive - 1 : alive;
-        // alive = cell === "true-old" ? alive - 1 : alive;
+        alive = cell === "true-old" ? alive - 1 : alive;
         // alive = cell === "true-mature" ? alive - 1 : alive;
         // Logic for status of new cell
         // 1. Check whether current cell is alive or dead (true or false)
@@ -103,16 +126,16 @@ class App extends React.Component {
             }
           }
           // 3. If true
-          // if ((cell === true || cell=="true-old") && done === false) {
-          if ((cell === true ) && done === false) {
-          if (alive <= 1 || alive >= 4) {
+          if ((cell === true || cell === "true-old") && done === false) {
+            // if ((cell === true) && done === false) {
+            if (alive <= 1 || alive >= 4) {
               cell = false;
               done = true;
-            } 
-            // else {
-            //   cell = "true-old";
-            //   done=true;
-            // }
+            }
+            else {
+              cell = "true-old";
+              done = true;
+            }
           }
 
           // if ((cell === "true-old" || cell == "true-mature") && done === false) {
