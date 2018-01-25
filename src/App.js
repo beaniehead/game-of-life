07f79cef8ace81004782793cdaa6d305e817b629
Dicky <17915329+beaniehead@users.noreply.github.com>
2018-01-25 11:44:31 -0500

    Updated Speed

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends React.Component {
       grid: [],
       gameStatus: "paused",
       gridSize: 35,
-      speed: 150
+      speed: 50
       //use to determine size of grid at start. New grid sizes stored here, but maybe different function to one in willmount, as new grid will be empty
       // maybe use nested loops and simple push new grid to array
     }
